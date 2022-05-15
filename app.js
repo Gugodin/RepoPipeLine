1ecfda656fe27bcc055502ce1ceb8477f7e88a48
Axel Ruiz L <developeraxrulo@gmail.com>
2022-05-15 16:03:17 -0500

    second version

--- app.js
+++ app.js
@@ -1,45 +1,17 @@
 var express = require('express');
 var app = express();
-const fetch = require("node-fetch")
 const port = process.env.PORT || 3000
+const pokemones = require("./routes/pokemons")
+const calculadora = require("./routes/operations")
+const files = require("./routes/files")
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/resta/:numero1/:numero2/', function (req, res) {
-  numero1 = parseInt(req.params.numero1)
-  numero2 = parseInt(req.params.numero2)
-  let resultado = numero1 - numero2
-  res.send({ resultado })
-
-});
-
-app.get('/multiplicacion/:numero1/:numero2/', function (req, res) {
-  numero1 = parseInt(req.params.numero1)
-  numero2 = parseInt(req.params.numero2)
-  let resultado = numero1 * numero2
-  res.send({ resultado })
-});
-
-app.get('/suma/:numero1/:numero2/', function (req, res) {
-  numero1 = parseInt(req.params.numero1)
-  numero2 = parseInt(req.params.numero2)
-  let resultado = numero1 + numero2
-  res.send({ resultado });
-});
-
-app.get('/pokemon/:id', function (req, res) {
-  id = req.params.id
-  url = `https://pokeapi.co/api/v2/pokemon/${id}/`
-  fetch(url)
-  .then(response => response.json())
-  .then(data => res.send(data));
-  
-});
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use("/api/pokemon",pokemones)
+app.use("/api/calculadora",calculadora)
+app.use("/",files)
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log(`funcinando en el puerto ${port}`)
 });
