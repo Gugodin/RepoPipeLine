6b8e62b3ef760c755e8932faca1d584b319361d7
Javier Gutierrez <gugodin@hotmail.com>
2022-06-29 22:43:34 -0500

    Primer commit

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000
-const pokemones = require("./routes/pokemons")
-const calculadora = require("./routes/operations")
+
 const files = require("./routes/files")
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use("/api/pokemon",pokemones)
-app.use("/api/calculadora",calculadora)
-app.use("/",files)
+
+
+
+app.get('/',(req,res)=>{
+  res.send({respuesta:'all ok'})
+})
+
+app.get('/prueba',(req,res)=>{
+  res.send({respuesta:'Soy la prueba'})
+})
+
+
 
 app.listen(port, function () {
   console.log(`funcinando en el puerto ${port}`)
